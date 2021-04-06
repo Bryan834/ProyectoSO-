d437f48fd32461669249b92104cbc50859b9c679
marc10 <bryangarciamartinUPC@gmail.com>
2021-04-07 00:49:17 +0200

    version nombre en mayuscula

--- Version1 Proyecto ClIENTE/WindowsFormsApplication1/Form1.Designer.cs
+++ Version1 Proyecto ClIENTE/WindowsFormsApplication1/Form1.Designer.cs
@@ -59,9 +59,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(27, 50);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 24);
+            this.label2.Size = new System.Drawing.Size(93, 24);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Usuario";
+            this.label2.Text = "USUARIO";
             // 
             // usuario
             // 
